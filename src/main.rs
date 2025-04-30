369a106ac82fe62d74c4d3004f48908fd9d9b298
Santiago Morales <s.moralesvranic@gmail.com>
2025-04-30 18:05:03 -0300

    Initial work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+enum Registers{
+    RR0,
+    RR1,
+    RR2,
+    RR3,
+    RR4,
+    RR5,
+    RR6,
+    RR7,
+    RPC,
+    RCOND,
+    RCOUNT
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Initialize empty memory
+    let mut memory = [0_u16;2_usize.pow(16)];
+    memory[0] = Registers::RPC as u16;
+    print!("{:?}",memory[0]);
 }
 
 #[cfg(test)]
